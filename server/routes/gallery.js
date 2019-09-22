d0729904bd42a0c6a78db63cbb5bc95c3c4ab7d9
Lewis Nozaki <lhnozaki@gmail.com>
2019-09-21 19:26:21 -1000

    put completed. Deleted next

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -5,8 +5,26 @@ const Gallery = require("../database/models/Gallery");
 const knex = require("../database/knex");
 const isAuthenticated = require("../database/isAuthenticated");
 
+router.put("/:id", isAuthenticated, (req, res, next) => {
+  return req.db.Gallery.where({ id: req.params.id })
+    .fetchAll()
+    .then(results => {
+      return new Gallery({ id: req.params.id })
+        .save(
+          {
+            description: req.body.description,
+            url: req.body.url,
+            user_id: req.user.id
+          },
+          { patch: false }
+        )
+        .then(results => {
+          res.status(200).redirect("/gallery");
+        });
+    });
+});
+
 router.post("/", isAuthenticated, (req, res, next) => {
-  console.log(req.body);
   return new Gallery({
     description: req.body.description,
     url: req.body.url,
@@ -26,14 +44,20 @@ router.get("/new", isAuthenticated, (re, res, next) => {
   res.status(200).render("new");
 });
 
+router.get("/:id/edit", isAuthenticated, (req, res, next) => {
+  return req.db.Gallery.where({ id: req.params.id })
+    .fetchAll()
+    .then(results => {
+      let picId = results.models.map(e => e.attributes.id);
+      res.status(200).render("edit", { id: [...picId] });
+    });
+});
+
 router.get("/:id", isAuthenticated, (req, res, next) => {
   return req.db.Gallery.where({ id: req.params.id })
     .fetchAll({ withrelated: ["user_id"] })
     .then(results => {
       let pics = results.models.map(e => e.attributes);
-
-      console.log(req.user.id);
-      console.log(pics[0].user_id);
 
       if (req.user.id !== pics[0].user_id) {
         req.db.User.where({ id: pics[0].user_id })
@@ -50,20 +74,25 @@ router.get("/:id", isAuthenticated, (req, res, next) => {
           .fetchAll()
           .then(user => {
             let username = user.models.map(e => e.attributes.username);
-
-            res
-              .status(200)
-              .render("picture", { picture: pics, username: [...username] });
+            let picId = results.models.map(e => e.attributes.id);
+            res.status(200).render("picture", {
+              picture: pics,
+              username: [...username],
+              id: [...picId]
+            });
           });
       }
     });
 });
 
 router.get("/", isAuthenticated, (req, res, next) => {
-  return req.db.Gallery.fetchAll().then(results => {
-    let pics = results.models.map(e => e.attributes);
-    res.status(200).render("gallery", { picture: pics });
-  });
+  return req.db.Gallery.forge()
+    .orderBy("id", "ASC")
+    .fetchAll()
+    .then(results => {
+      let pics = results.models.map(e => e.attributes);
+      res.status(200).render("gallery", { picture: pics });
+    });
 });
 
 module.exports = router;
